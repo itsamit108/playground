127eb37ecf0624b67800577c959da889c5efa74d
Amit <111522057+itsamit108@users.noreply.github.com>
2023-03-13 16:34:46 +0000

    Mar 13, 2023, 10:04 PM

--- Django/demo/app/views.py
+++ Django/demo/app/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def about(request):
+    return HttpResponse("This is the about page.")
