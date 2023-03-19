8f1466bfe472450b223c002790ae674d87a8f5b0
itsamit108 <itsamit108@outlook.com>
2023-03-19 17:01:06 +0000

    Mar 19, 2023, 10:31 PM

--- Django/myproj/myproj/urls.py
+++ Django/myproj/myproj/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "My Admin"
+admin.site.site_title = "My Admin Portal"
+admin.site.index_title = "Welcome to My Portal"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("myapp.urls")),
 ]
