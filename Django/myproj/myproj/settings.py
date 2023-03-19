a36103949930d4cbeae4f82cf75af2ca810f7db0
itsamit108 <itsamit108@outlook.com>
2023-03-19 16:01:23 +0000

    Mar 19, 2023, 9:31 PM

--- Django/myproj/myproj/settings.py
+++ Django/myproj/myproj/settings.py
@@ -126,6 +126,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Added manually
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
+    os.path.join(BASE_DIR, "static"),
 ]
