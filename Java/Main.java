4acfa7aa43bf9733b2dc167ea350f7ac43aff181
itsamit108 <itsamit108@outlook.com>
2023-03-27 09:23:41 +0000

    Mar 27, 2023, 2:53 PM

--- Java/Main.java
+++ Java/Main.java
@@ -1,20 +1,7 @@
 class Demo {
-    void join(int a, int b){
-        System.out.println(a+b);
-    }
-    void join(double a, double b){
-        System.out.println(a+b);
-    }
-    void join(String a, String b){
-        System.out.println(a+b);
-    }
 }
 
 class Main {
     public static void main(String[] args) {
-        Demo obj1=new Demo();
-        obj1.join(1,2);
-        obj1.join(1.5,2.5);
-        obj1.join("Hello ", "World");
     }
 }
