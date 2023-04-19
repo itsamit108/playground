03ad5cdeda189950cbd00f7fffde3e1d3253b1bc
itsamit108 <itsamit108@outlook.com>
2023-04-19 15:12:33 +0000

    Apr 19, 2023, 8:42 PM

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,32 @@
-class Main{
-public static void main(String[] args) {
-    
+class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    int age;
+
+    Dog(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    void eat() {
+        super.eat(); // calls the Animal's eat() method
+        System.out.println("Dog is eating");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Tommy", 5);
+        dog.eat();
     }
 }
