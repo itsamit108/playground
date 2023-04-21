c36c24c554233031eba98a345978c16c0ddd73fe
itsamit108 <itsamit108@outlook.com>
2023-04-21 15:57:08 +0000

    Apr 21, 2023, 9:27 PM

--- experiment.cpp
+++ experiment.cpp
@@ -1,15 +1,38 @@
-#include<bits/stdc++.h> // bits/stdc++.h should not be used in production.
+#include <bits/stdc++.h> // bits/stdc++.h should not be used in production.
 
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node *next;
+    Node *next;
 };
 
 int main()
 {
-    
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
 
     return 0;
 }
