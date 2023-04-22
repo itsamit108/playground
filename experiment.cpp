afbefb3a179505a0bc6180cbf17a9955713ea8fb
itsamit108 <itsamit108@outlook.com>
2023-04-22 06:19:52 +0000

    Apr 22, 2023, 11:49 AM

--- experiment.cpp
+++ experiment.cpp
@@ -1,10 +1,38 @@
-#include<bits/stdc++.h> // bits/stdc++.h should not be used in production.
+#include <bits/stdc++.h> // bits/stdc++.h should not be used in production.
 
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        data = 0;
+        next = nullptr;
+    }
+};
+
+class LinkedList
+{
+    public:
+    Node *head;
+    LinkedList()
+    {
+        head = nullptr;
+    }
+    void add(int data)
+    {
+        Node *newNode = new Node();
+        newNode->data = data;
+        newNode->next = head;
+        head = newNode;
+    }
+};
+
 int main()
 {
-    
 
     return 0;
 }
