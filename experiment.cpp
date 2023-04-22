80a32f84a8db32df76a4d5dd51f134746d076b75
itsamit108 <itsamit108@outlook.com>
2023-04-22 06:05:46 +0000

    Apr 22, 2023, 11:35 AM

--- experiment.cpp
+++ experiment.cpp
@@ -1,34 +1,10 @@
-#include <iostream>
-#include <forward_list>
+#include<bits/stdc++.h> // bits/stdc++.h should not be used in production.
 
 using namespace std;
 
 int main()
 {
-    forward_list<int> myList = {1, 2, 3, 4, 5};
-
-    // Print the list
-    for (auto i : myList)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    // Insert an element at the beginning of the list
-    myList.push_front(0);
-
-    // Insert an element after the first element of the list
-    myList.insert_after(myList.begin(), 6);
-
-    // Erase an element from the list
-    myList.erase_after(++myList.begin());
-
-    // Print the modified list
-    for (auto i : myList)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
+    
 
     return 0;
 }
