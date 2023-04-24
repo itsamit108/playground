8663b0a9b6611b41f298dc182e121fa92e02b088
itsamit108 <itsamit108@outlook.com>
2023-04-24 09:22:01 +0000

    Apr 24, 2023, 2:52 PM

--- myenv/myproj/myapp/views.py
+++ myenv/myproj/myapp/views.py
@@ -1,28 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 import datetime
-
-# Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
-
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    today = datetime.datetime.now().date()
+    template = loader.get_template("appname/index.html")
+    context = {"today": today}
+    return HttpResponse(template.render(context, request))
