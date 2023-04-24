4f261be88596adfc75c7632a77df7e08f04741c2
itsamit108 <itsamit108@outlook.com>
2023-04-24 09:46:05 +0000

    Apr 24, 2023, 3:16 PM

--- myenv/myproj/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import datetime
-
-
-def index(request):
-    today = datetime.datetime.now().date()
-    days = [1, 2, 3, 4, 5]  # just an example list of days
-    context = {"today": today, "days": days}
-    return render(request, "myapp/index.html", context)
