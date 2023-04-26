83e5edd054da8aa58825c76e177fa4bf41db75ca
itsamit108 <itsamit108@outlook.com>
2023-04-26 05:11:56 +0000

    Apr 26, 2023, 10:41 AM

--- experiment.cpp
+++ experiment.cpp
@@ -1,42 +1,44 @@
-#include<bits/stdc++.h> // bits/stdc++.h should not be used in production.
+#include <bits/stdc++.h> // bits/stdc++.h should not be used in production.
 
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
-    Node(){
-        data=0;
+    Node()
+    {
+        data = 0;
         next = nullptr;
     }
 };
 
-Node* fn(Node* head, int n){
-    Node* curr = head;
+Node *fn(Node *head, int n)
+{
+    Node *curr = head;
     Node *prev = nullptr;
-    int c=1;
+    int c = 1;
     int l = 0;
-    while (curr->next!=nullptr)
+    while (curr != nullptr)
     {
         curr = curr->next;
         l++;
     }
 
-    while (curr!=nullptr && c<l-n+1)
+    while (curr != nullptr && c < l - n + 1)
     {
         curr = curr->next;
         prev = curr;
         c++;
     }
-    prev->next = curr -> next;
+    prev->next = curr->next;
     delete curr;
     return head;
 }
 
 int main()
 {
-
 
     return 0;
 }
