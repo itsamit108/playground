80210fca302868daaf7c4f3340300a8e8c2269f5
itsamit108 <itsamit108@outlook.com>
2023-04-26 11:24:29 +0000

    Apr 26, 2023, 4:54 PM

--- Java/Main.java
+++ Java/Main.java
@@ -1,10 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        try {
-            int[] arr = new int[3];
-            arr[3] = 10;
-        } catch (Exception e) {
-            System.out.println("An exception occurred: " + e.getMessage());
-        }
+class Main{
+public static void main(String[] args) {
+    
     }
 }
