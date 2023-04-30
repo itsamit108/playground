2e4b43329745fb1010c4d7dd035d3dc1ea6d5067
itsamit108 <itsamit108@outlook.com>
2023-04-30 12:25:39 +0000

    Apr 30, 2023, 5:55 PM

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,26 +1,23 @@
-// Binary search
+function binarySearch(array: number[], target: number): number {
+    let left = 0;
+    let right = array.length - 1;
 
-function binarySearch<T>(array: T[], value: T): number {
-  let low = 0;
-  let high = array.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
 
-  while (low <= high) {
-    const mid = Math.floor((low + high) / 2);
-    const element = array[mid];
-
-    if (element < value) {
-      low = mid + 1;
-    } else if (element > value) {
-      high = mid - 1;
-    } else {
-      return mid;
+        if (array[mid] === target) {
+            return mid;
+        } else if (array[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-  }
 
-  return -1;
+    return -1;
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const value = 5;
+const arr: number[] = [1, 3, 5, 7, 9];
+const target: number = 5;
 
-console.log(binarySearch(array, value));
+console.log(binarySearch(arr, target)); // Output: 2
