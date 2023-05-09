0e0496f1e33b87dd07fa6c120782abdd7610b326
itsamit108 <itsamit108@outlook.com>
2023-05-09 12:49:13 +0000

    May 9, 2023, 6:19 PM

---
+++ todo-app-starter-code/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCW5tQePdcbk_AlVsj9uUTHKaIxOTKCQss",
+    authDomain: "fir-29b8a.firebaseapp.com",
+    projectId: "fir-29b8a",
+    storageBucket: "fir-29b8a.appspot.com",
+    messagingSenderId: "296775505246",
+    appId: "1:296775505246:web:22ce75d8dbeb7bf7f82bcf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
