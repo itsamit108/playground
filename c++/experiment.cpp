443ec5ddfbddf3f4beae612858882a18d3fe470d
itsamit108 <itsamit108@outlook.com>
2023-05-12 06:34:00 +0000

    May 12, 2023, 12:04 PM

--- c++/experiment.cpp
+++ c++/experiment.cpp
@@ -7,18 +7,22 @@ int main()
     int r = 0, c = 0;
     int mat[r][c];
 
-    for (int i = 0; i < r;i++){
-        for (int j = 0; j < c;j++){
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
             mat[r][c] = i + j;
         }
     }
 
-    for (int i = 0; i < r;i++){
-        for (int j = 0; j < c;j++){
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
             cout << mat[r][c];
         }
         cout << endl;
     }
 
-        return 0;
+    return 0;
 }
