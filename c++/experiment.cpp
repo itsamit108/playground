63f2584513c4bc6b9df231fa486311c3a99967bc
itsamit108 <itsamit108@outlook.com>
2023-05-12 06:32:57 +0000

    May 12, 2023, 12:02 PM

---
+++ c++/experiment.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> // bits/stdc++.h should not be used in production.
+
+using namespace std;
+
+int main()
+{
+    int r = 0, c = 0;
+    int mat[r][c];
+
+    for (int i = 0; i < r;i++){
+        for (int j = 0; j < c;j++){
+            mat[r][c] = i + j;
+        }
+    }
+
+    for (int i = 0; i < r;i++){
+        for (int j = 0; j < c;j++){
+            cout << mat[r][c];
+        }
+        cout << endl;
+    }
+
+        return 0;
+}
