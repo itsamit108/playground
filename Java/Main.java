ee4c511d4461b20dc4ba9f2f2c11b99e3c8f7d9a
itsamit108 <itsamit108@outlook.com>
2023-05-26 12:13:43 +0000

    May 26, 2023, 5:43 PM

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,14 @@
-class Main {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        JFrame frame = new JFrame("Label Example");
+        ImageIcon icon = new ImageIcon("path_to_image.jpg");
+        JLabel label = new JLabel("Hello, World!", icon, JLabel.CENTER);
+        frame.add(label);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
