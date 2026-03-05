da2ebc681b01608b36c4ad1a248da3fef3974345
Amit <itsamit108@gmail.com>
2026-03-05 18:43:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome to the Playground
+
+Everything organic, everything self contained
---
+++ SystemThinking&Programming/cloud&devops/py/main.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Configuration from .env
+AWS_ENDPOINT_URL = os.getenv("AWS_ENDPOINT_URL", "http://localhost:4566")
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "test")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "test")
